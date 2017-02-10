d6dedbe6c3345c6729cb8fcaae8cb93b666df89f
David Beer <david.m.beer@gmail.com>
2017-02-10 19:04:39 +0000

    Test Structure Setup

--- src/test/java/com/copperarrow/TestHomePage.java
+++ src/test/java/com/copperarrow/TestHomePage.java
@@ -1,28 +1,45 @@
 package com.copperarrow;
 
+import com.copperarrow.auth.config.SecurityWebApplicationInitializer;
 import org.apache.wicket.util.tester.WicketTester;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.security.test.context.support.WithAnonymousUser;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
  * Simple test using the WicketTester
  */
-public class TestHomePage
-{
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = SecurityWebApplicationInitializer.class)
+@WithAnonymousUser
+public class TestHomePage extends WicketApplicationTest {
 	private WicketTester tester;
+	@Autowired
+	private ApplicationContext ctx;
 
 	@Before
-	public void setUp()
-	{
-		tester = new WicketTester(new WicketApplication());
+	public void setUp() throws Exception {
+		super.setUp();
+		tester = getTester();
+		tester.startPage(HomePage.class);
+	}
+
+	/**
+	 * Subclasses can use this method to provide the configuration needed by
+	 * each test.
+	 */
+	@Override
+	protected void setupTest() {
+
 	}
 
 	@Test
-	public void homepageRendersSuccessfully()
-	{
-		//start and render the test page
-		tester.startPage(HomePage.class);
-
+	public void homepageRendersSuccessfully() {
 		//assert rendered page class
 		tester.assertRenderedPage(HomePage.class);
 	}
---
+++ src/test/java/com/copperarrow/WicketApplicationTest.java
@@ -0,0 +1,83 @@
+package com.copperarrow;
+
+import org.apache.wicket.Page;
+import org.apache.wicket.protocol.http.WebApplication;
+import org.apache.wicket.spring.injection.annot.SpringComponentInjector;
+import org.apache.wicket.spring.test.ApplicationContextMock;
+import org.apache.wicket.util.tester.WicketTester;
+import org.junit.Before;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by dbeer on 15/01/17.
+ */
+@Component
+public abstract class WicketApplicationTest {
+
+    protected static final String USERDAO_BEAN_NAME = "UserDAO";
+    protected static final String CUSTOM_USER_DETAILS = "customUserDetailsService";
+    private ApplicationContextMock applicationContextMock;
+
+    private WicketTester tester = null;
+
+    @Before
+    public void setUp() throws Exception {
+        //Creates a new application context mock.
+        applicationContextMock = new ApplicationContextMock();
+
+        //Creates a new WicketTester
+        tester = new WicketTester(newWebApplication());
+        setupTest();
+    }
+
+    protected WebApplication newWebApplication() {
+        return new WebApplication() {
+
+            @Override
+            protected void init() {
+                super.init();
+
+                getMarkupSettings().setStripWicketTags(false);
+                //Configures the SpringBean annotation support to use the mock application context.
+                //This ensures that the mock objects are injected instead of the actual bean classes.
+                getComponentInstantiationListeners().add(new SpringComponentInjector(this, applicationContextMock));
+                WicketApplicationTest.this.init(this);
+            }
+
+            @Override
+            public Class<? extends Page> getHomePage() {
+                return WicketApplicationTest.this.getHomePage();
+            }
+
+        };
+    }
+
+    protected void init(WebApplication app) {}
+
+    protected Class<? extends Page> getHomePage() {
+        return Page.class;
+    }
+
+    /**
+     * Subclasses can use this method to provide the configuration needed by
+     * each test.
+     */
+    protected abstract void setupTest();
+
+    /**
+     * Adds mock to the mock application context.
+     * @param beanName  The name of the mock bean.
+     * @param mock  The mock object.
+     */
+    protected void addMock(String beanName, Object mock) {
+        applicationContextMock.putBean(beanName, mock);
+    }
+
+    protected ApplicationContextMock getApplicationContextMock() {
+        return applicationContextMock;
+    }
+
+    protected WicketTester getTester() {
+        return tester;
+    }
+}
